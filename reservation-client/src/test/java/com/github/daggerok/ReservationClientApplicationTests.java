b9410f4eb149d4b72500164c9ca93ee229ac24a9
Maksim Kostromin <daggerok@gmail.com>
2019-04-11 21:20:07 +0300

    Init client draft.

---
+++ reservation-client/src/test/java/com/github/daggerok/ReservationClientApplicationTests.java
@@ -0,0 +1,9 @@
+package com.github.daggerok;
+
+import org.junit.Test;
+
+public class ReservationClientApplicationTests {
+
+  @Test
+  public void contextLoads() { }
+}
--- reservation-service/src/main/java/com/github/daggerok/domain/ReservationReactiveMongoRepository.java
+++ reservation-service/src/main/java/com/github/daggerok/domain/ReservationReactiveMongoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.query.Param;
 import reactor.core.publisher.Flux;
 
 public interface ReservationReactiveMongoRepository extends ReactiveMongoRepository<Reservation, String> {
-  Flux<Reservation> findAllByName(@Param("name") String nane);
+  Flux<Reservation> findAllByName(@Param("name") String name);
 }
